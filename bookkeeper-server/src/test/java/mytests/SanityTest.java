51fa8ced641d0e60b6a3b6450e9a95450877e9a1
Denni-02 <dennis.mariani.15@gmail.com>
2025-06-03 17:15:25 +0200

    Configurazione progetto per testing ISW2: rimossi test nativi, attivata struttura student-test

---
+++ bookkeeper-server/src/test/java/mytests/SanityTest.java
@@ -0,0 +1,12 @@
+package mytests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SanityTest {
+
+    @Test
+    public void testAlwaysPasses() {
+        assertTrue(true);
+    }
+}
